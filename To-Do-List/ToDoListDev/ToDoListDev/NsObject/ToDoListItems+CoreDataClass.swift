b84be754a9e610bc1e026ec4fe629c0568723b82
Pabitra Ranjan Sahoo <138593503+Pabitra-git@users.noreply.github.com>
2023-12-13 09:29:12 +0530

    Add files via upload
    An application to note down the incomplete works made using swift language.

---
+++ To-Do-List/ToDoListDev/ToDoListDev/NsObject/ToDoListItems+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItems+CoreDataClass.swift
+//  ToDoListDev
+//
+//  Created by Pabitra Ranjan Sahoo on 05/12/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItems)
+public class ToDoListItems: NSManagedObject {
+
+}
